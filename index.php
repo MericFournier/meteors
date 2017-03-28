f8855fb239cc18a15ba1ac8f9691b1141faa7345
MericFournier <meric.fournier@hetic.net>
2017-03-28 15:54:45 +0200

    mise à jour

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <?php
 
 $url  = 'https://ssd-api.jpl.nasa.gov/fireball.api';
@@ -17,51 +18,73 @@ $meteors = json_decode($meteors);
       <title>Document</title>
       <script src="http://www.webglearth.com/v2/api.js"></script>
 
-   <style>
-      
-      .div { 
-         width:100px;
-         height:30px;
-         border-radius:20px;
-         border:1px solid black;
-         margin:20px;
-         display:inline-block;
-      }
+      <style>
+
+         .div { 
+            width:100px;
+            height:30px;
+            border-radius:20px;
+            border:1px solid black;
+            margin:20px;
+            display:inline-block;
+         }
       </style>
-      
+
    </head>
    <body>
 
       <?php foreach($meteors->data as $meteor): ?>
-      <div class="div" data-date="<?=  $meteor[0] ?>" data-country =" <?= $meteor[3].' '.$meteor[4].' '.$meteor[5].' '.$meteor[6] ?> ">
-         
+      <div class="div" data-country=" " data-date="<?=  $meteor[0] ?>" data-lat ="<?= $meteor[3]?>" data-lon ="<?= $meteor[5]?>">
+
       </div>
       <?php endforeach ?>
 
    </body>
-   <!-- Obtenir la population dans le pays cliqué --> 
+
+  
+
+
    <script>
 
+      var country;
       var population;
-
-      var div = document.querySelectorAll('.test')
+      var div = document.querySelectorAll('.div')
       console.log(div)
       for ( var i = 0; i<div.length; i++ ) {
          div[i].onclick = function() {
             var that = this
-            var data = this.dataset.country
-            fetch( 'http://api.population.io:80/1.0/population/'+data+'/2016-12-31/' )
-               .then( ( response ) =>
-                     {
+            var lat = this.dataset.lat
+            var lon = this.dataset.lon
+            console.log(lat,lon);
+            fetch( 'http://ws.geonames.org/countryCodeJSON?lat='+lat+'&lng='+lon+'&username=meric' )
+            .then( ( response ) =>
+            {
                return response.json()
             } )
-               .then( ( result ) =>
-                     {
-               that.innerHTML = result.total_population.population
-
+            .then( ( result ) =>
+            {
+               if ( result = 'undefined' ) { return false; }
+               return fetch( 'http://api.population.io:80/1.0/population/'+result.countryName+'/2016-12-31/' )
+            } )
+            .then( ( response ) =>
+            {
+               return response.json()
+            } )
+            .then( ( result ) =>
+            {
+               console.log(result.total_population.population);
+               that.innerHTML = result.total_population.population;
             } ) 
-         }
+
+            }
+
+
+
+
+
+
       }
+      
 
    </script>
 </html>
